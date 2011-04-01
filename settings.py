98520ee0438aa2b469f9b168bdb479b64f93f0df
zhangdongmao <deanraccoon@gmail.com>
2011-04-01 18:11:32 +0800

    email

--- settings.py
+++ settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/zhangdongmao/newsmth/smthtop10/sqlite.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'django',                      # Or path to database file if using sqlite3.
+        'USER': 'django',                      # Not used with sqlite3.
+        'PASSWORD': 'django',                  # Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
--- smthtop10/sendEmail.py
+++ smthtop10/sendEmail.py
@@ -3,15 +3,13 @@ sys.path.append("..")
 sys.path.append("../..")
 from smthtop10.models import UserProfile
 from django.core import mail
-messages=[]
 t=mail.EmailMessage()
 for i in UserProfile.objects.all():
-    t.to.append(i.kindlemail)
-    print i.kindlemail
-t.from_email="admin@pushkindle.com"
+    t.to.append("deanraccoon@kindle.com")
+print t.to
+t.from_email="admin@localhost"
+t.subject="Convert"
+t.body="sm.mobi"
 t.attach_file("/home/zhangdongmao/newsmth/smthtop10/archive/20110331/sm.mobi")
-messages.append(t)
-connection = mail.get_connection()
-connection.open()
-connection.send_messages(messages)
-connection.close()
+t.send()
+
