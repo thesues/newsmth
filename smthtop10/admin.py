4f1a9068da8296c41c896d4544d85ec8c84a5b7e
zhangdongmao <deanraccoon@gmail.com>
2011-04-26 09:26:08 +0800

    add admin updatetime

--- smthtop10/admin.py
+++ smthtop10/admin.py
@@ -2,5 +2,5 @@ from smthtop10.models import *
 from django.contrib import admin
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display=["user","kindlemail"]
+    list_display=["user","kindlemail","updateTime"]
 admin.site.register(UserProfile,UserProfileAdmin)
