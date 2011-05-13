58c41710e024dcd75e3414785079243f6448f4e0
zhangdongmao <deanraccoon@gmail.com>
2011-05-13 16:19:36 +0800

    add css/html/images/ media

--- settings.py
+++ settings.py
@@ -45,7 +45,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/zhangdongmao/newsmth/smthtop10/archive/'
+MEDIA_ROOT = '/home/zhangdongmao/newsmth/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
