be2d83a60fc57f3967cea60332ef8f07afc9dda2
zhangdongmao <deanraccoon@gmail.com>
2011-05-14 03:24:16 +0800

    no needs to activation mail

--- smthtop10/regbackend.py
+++ smthtop10/regbackend.py
@@ -5,6 +5,9 @@ def user_created(sender, user, request, **kwargs):
     profile=UserProfile(user=user)
     profile.kindlemail=form.data['kindlemail']
     profile.updateTime=form.data['updateTime']
+    user.is_active=True
+    user.save()
+    profile.activation_key="ALREADY_ACTIVATED"
     profile.save()
 from registration.signals import user_registered
 user_registered.connect(user_created)
