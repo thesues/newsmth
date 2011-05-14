17a3b2494ba036dc3398d93ac9db2b38c1433853
zhangdongmao <deanraccoon@gmail.com>
2011-05-14 13:38:59 +0800

    add registration model

--- smthtop10/regbackend.py
+++ smthtop10/regbackend.py
@@ -1,13 +1,11 @@
 from registration.forms import RegistrationForm
 from forms import *
+import pdb
 def user_created(sender, user, request, **kwargs):
     form = UserRegsiterForm(request.POST)
     profile=UserProfile(user=user)
     profile.kindlemail=form.data['kindlemail']
     profile.updateTime=form.data['updateTime']
-    user.is_active=True
-    user.save()
-    profile.activation_key="ALREADY_ACTIVATED"
     profile.save()
 from registration.signals import user_registered
 user_registered.connect(user_created)
